e6b2f8513dddfdc48ab7d77894c7c057a94b3345
peco-tanaka <ok.computeruk1929@gmail.com>
2025-11-28 00:59:48 +0900

    feat: offset変数を作成してカメラ初期位置を変数化

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,16 +3,15 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Vector3 offset = new Vector3(0, 5, -7);
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         // OFFSET THE CAMERA BEHIND THE PLAYER BY Adding to the player's position
-        transform.position = player.transform.position + new Vector3(0, 5, -7);
+        transform.position = player.transform.position + offset;
     }
 }
