13e960835649df8cd3716c9d8bcc275b9e0c0a44
peco-tanaka <ok.computeruk1929@gmail.com>
2025-11-28 02:43:44 +0900

    feat: 水平方向のボタン移動機能を追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float turnSpeed;
+    public float horizontalInput;
     void Start()
     {
         
@@ -13,8 +14,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
         // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
